d0a3c2faa8030cb390f0b3820365541e366d34a5
Katherine Guarnizo <guarnizo.katherine.5@gmail.com>
2023-04-30 20:55:30 -0400

    Create post  method

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,22 @@
-from flask import Blueprint, jsonify, abort, make_response
+from app import db
+from app.models.planet import Planet
+from flask import Blueprint, jsonify, abort, make_response, request
 
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
+
+@planets_bp.route("", methods=["POST"])
+def create_planets():
+    request_body = request.get_json()
+    new_planet = Planet(name = request_body["name"],
+        description = request_body["description"],
+        num_moons = request_body["num_moons"])
+    
+    db.session.add(new_planet)
+    db.session.commit()
+
+    return make_response(f"Planet {new_planet.name} successfully created, 201")
+
+
 
 # class Planet:
 #     def __init__(self, id, name, description, num_moons):
