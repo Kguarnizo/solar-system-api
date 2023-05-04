026110ab74d14c98c847d377ebbb475a90572ff8
Madison Jackson <madi.jackson29@gmail.com>
2023-05-04 11:35:51 -0700

    refactoring; update model with classmethod

--- app/routes.py
+++ app/routes.py
@@ -7,9 +7,7 @@ planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 @planets_bp.route("", methods=["POST"])
 def create_planets():
     request_body = request.get_json()
-    new_planet = Planet(name = request_body["name"],
-        description = request_body["description"],
-        num_moons = request_body["num_moons"])
+    new_planet = Planet.from_dict(request_body)
     
     db.session.add(new_planet)
     db.session.commit()
@@ -18,17 +16,8 @@ def create_planets():
 
 @planets_bp.route("", methods=["GET"])
 def read_all_planets():
-    planets_response = []
     planets = Planet.query.all()
-    for planet in planets:
-        planets_response.append(
-            {
-                "id": planet.id,
-                "name": planet.name,
-                "description": planet.description,
-                "num_moons": planet.num_moons
-            }
-        )
+    planets_response = [planet.to_dict() for planet in planets]
     return jsonify(planets_response)
 
 
@@ -36,12 +25,7 @@ def read_all_planets():
 def read_one_planet(planet_id):
     planet = validate_planet(planet_id)
 
-    return jsonify({
-                "id": planet.id,
-                "name": planet.name,
-                "description": planet.description,
-                "num_moons": planet.num_moons
-                })
+    return jsonify(planet.to_dict())
                 
 
 @planets_bp.route("/<planet_id>", methods=["PUT"])
